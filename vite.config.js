43b3a74355ba25723ea9291d44b94c6aa9ae91d7
senzi <sennes111762@gmail.com>
2026-03-12 10:12:34 +0800

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 设为相对路径以适应任何子路径部署
   plugins: [
     vue(),
     basicSsl() // 注入这个小魔法 w
